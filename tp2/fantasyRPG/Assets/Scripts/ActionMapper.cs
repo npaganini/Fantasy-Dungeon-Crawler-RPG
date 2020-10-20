3af5026169e2cd0152533d2b014eba20b3df813d
ggordyn <ggordyn@itba.edu.ar>
2020-10-19 21:37:51 -0300

    player logic and mapper scripts (incomplete)

---
+++ tp2/fantasyRPG/Assets/Scripts/ActionMapper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMapper : MonoBehaviour
+{
+    public static bool GetMoveLeft()
+    {
+        return Input.GetKey(KeyCode.A);
+    }
+
+    public static bool GetMoveRight()
+    {
+        return Input.GetKey(KeyCode.D);
+    }
+
+    public static bool GetMoveUp()
+    {
+        return Input.GetKey(KeyCode.W);
+    }
+
+    public static bool GetMoveDown()
+    {
+        return Input.GetKey(KeyCode.S);
+    }
+}
---
+++ tp2/fantasyRPG/Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    public CharacterController cc;
+    public float speed = 6f;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ReadInput();
+        UpdatePosition();
+    }
+
+    void ReadInput()
+    {
+
+    }
+
+    void UpdatePosition()
+    {
+
+    }
+}
