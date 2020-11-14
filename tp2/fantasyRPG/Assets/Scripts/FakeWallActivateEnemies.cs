b1f3378eb280add81dd74986474b0bd6f042efd6
jcgrethe <jgrethe@itba.edu.ar>
2020-11-14 18:36:39 -0300

    enemies in cave

---
+++ tp2/fantasyRPG/Assets/Scripts/FakeWallActivateEnemies.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FakeWallActivateEnemies : MonoBehaviour
+{
+    
+    public List<EnemyManager> enemiesToActivate;
+    
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.gameObject.CompareTag("Player"))
+        {
+            foreach (var enemy in enemiesToActivate)
+            {
+                enemy.Activate();
+            }
+        }
+    }
+}
