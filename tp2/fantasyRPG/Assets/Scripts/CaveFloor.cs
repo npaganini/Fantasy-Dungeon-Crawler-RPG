79d9bc685bb920d0c7c20be41d4a8c630686556f
jcgrethe <jgrethe@itba.edu.ar>
2020-11-14 17:24:43 -0300

    add floor script on cave

---
+++ tp2/fantasyRPG/Assets/Scripts/CaveFloor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveFloor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.gameObject.CompareTag("Player"))
+        {
+            col.gameObject.GetComponent<PlayerLogic>().Attacked(100);
+        } else if (col.transform.gameObject.CompareTag("Enemy"))
+        {
+            col.gameObject.GetComponent<EnemyManager>().Kill();
+        }
+        
+    }
+}
